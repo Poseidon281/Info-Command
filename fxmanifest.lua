d9f0cf8b4a52b16a89b61a38a5d18c5b99a8a5fc
Poseidon <62171904+Poseidon281@users.noreply.github.com>
2024-01-11 15:44:44 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,10 @@
-shared_script '@WaveShield/resource/waveshield.lua' --this line was automatically written by WaveShield
-
 fx_version 'cerulean'
 game 'gta5'
 
-name "BHL-Charinfo"
-description "BHL-Charinfo is een krachtige FiveM resource die is ontworpen om de spelerservaring te verbeteren door gedetailleerde informatie weer te geven over het personage (character) waarmee een speler interacteert. Met deze resource kunnen spelers snel en gemakkelijk toegang krijgen tot belangrijke informatie over andere personages, zoals hun naam, groepslidmaatschap, financiële status, inventaris en meer."
+name "Charinfo"
+description "Charinfo is een krachtige FiveM resource die is ontworpen om de spelerservaring te verbeteren door gedetailleerde informatie weer te geven over het personage (character) waarmee een speler interacteert. Met deze resource kunnen spelers snel en gemakkelijk toegang krijgen tot belangrijke informatie over andere personages, zoals hun naam, groepslidmaatschap, financiële status, inventaris en meer."
 author "Senna"
-version "1.3.0"
+version "1.3.4"
 
 shared_scripts {
 	'shared/*.lua'
