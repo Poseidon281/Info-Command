6b89e98cc5121f0181217d071a93b751349fdbf5
Poseidon <62171904+Poseidon281@users.noreply.github.com>
2024-01-14 22:10:26 +0100

    Update config.lua
    Verkeerde Webhook ingesteld. Graag de vorige webhook niet gebruiken.

--- shared/config.lua
+++ shared/config.lua
@@ -3,7 +3,7 @@ Config = {}
 
 ------------------- Info About Player Command -------------------
 Config.InfoCommand = "charinfo" -- De command
-Config.Infowebhook = "https://discord.com/api/webhooks/1194952168281739274/oqS1Usmr-GxfRARKVrUDPU5Gj8V2qqMQSh2aDjBe5bBP-nHQm6Si5S46wIC6obErOUoZ" -- De Discord Webhook
+Config.Infowebhook = "" -- De Discord Webhook
 Config.AvatarURL = 'https://cdn.discordapp.com/attachments/1194690726311768084/1194952007497293904/Helmond_Roleplay.png?ex=65b238c7&is=659fc3c7&hm=b053a57a9ce75117e3a7222b61f8ad2467cb557a0d16b1afaf6cb93f2f5369bf&' -- Het logo van je server
 Config.Server = "" -- Bijvoorbeeld: "Helmond Roleplay"
 Config.Color = 16743936 -- De kleur van de webhook
